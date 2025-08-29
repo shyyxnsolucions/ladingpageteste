7b1a14f2e97cef822b3e4f9ba5fd4cd9dcbeb8cd
shyyxn <gabrielsantoss836624@gmail.com>
2025-08-29 15:49:32 -0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import VideoSection from '@/components/VideoSection'
-import Testimonials from '@/components/Testimonials'
-import Benefits from '@/components/Benefits'
-import Ingredient from '@/components/Ingredient'
-import BeforeAfter from '@/components/BeforeAfter'
-import Pricing from '@/components/Pricing'
-import FAQ from '@/components/FAQ'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import VideoSection from '../components/VideoSection'
+import Testimonials from '../components/Testimonials'
+import Benefits from '../components/Benefits'
+import Ingredient from '../components/Ingredient'
+import BeforeAfter from '../components/BeforeAfter'
+import Pricing from '../components/Pricing'
+import FAQ from '../components/FAQ'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
